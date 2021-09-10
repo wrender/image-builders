66a34383d289e74aa52ab4ea2742dfd2f4a38395
root <root@localhost.localdomain>
2021-09-10 08:23:27 -0400

    disable salt minion

--- Ubuntu-20.04/ubuntu-chroot.sh
+++ Ubuntu-20.04/ubuntu-chroot.sh
@@ -69,7 +69,7 @@ sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.sal
 # Create apt sources list file
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee /etc/apt/sources.list.d/salt.list
 # Install Packages
-apt-get update && apt-get install -y salt-minion && systemctl enable salt-minion
+apt-get update && apt-get install -y salt-minion
 # Point to master
 cat <<EOF > /etc/salt/minion.d/settings.conf
 master: my.salt.masterhost
