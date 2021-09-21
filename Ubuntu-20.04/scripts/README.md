227e6b06e5545158829ac6a2e25c8f594a7d8ea7
wes <wes@otherdata.com>
2021-09-21 10:57:07 +0000

    add scripts

---
+++ Ubuntu-20.04/scripts/README.md
@@ -0,0 +1,62 @@
+# Download Current Pre-Built ISO
+[ubuntu-20.04.3-live-server-salt-amd64.iso](https://www.otherdata.com/custom-images/ubuntu-20.04.3-live-server-salt-amd64.iso) (docker, salt-minion) | md5sum: a38c4d7f10d53463d2006e5948b5bf71
+
+# Create the ISO
+### Requirements
+- An Ubuntu 20.04 Desktop or Server to run the script on
+### Steps
+- Download the scripts folder
+
+# Option 2 - Download, Extract, Edit, and Recreate the ISO
+
+Download the ISO from the link above.
+
+## Extract ISO
+```
+cd tmp;
+xorriso -osirrox on -indev /root/ubuntu-custom/ubuntu-20.04.3-live-server-salt-amd64.iso -extract / /tmp/tmpiso
+```
+
+## Unsquash Filesystem
+```
+unsquashfs -f -d /tmp/tmpunsquash /tmp/tmpiso/casper/filesystem.squashfs
+```
+
+## Edit Salt Master Settings
+```
+vim /tmp/tmpunsquash/etc/salt/minion.d/settings.conf
+```
+## Resquash Filesytem
+```
+rm -rf /tmp/tmpiso/casper/filesystem.squashfs
+mksquashfs /tmp/tmpunsquash /tmp/tmpiso/casper/filesystem.squashfs
+```
+
+## Re-Create ISO
+```
+cd /tmp/tmpiso;
+xorriso \
+   -as mkisofs \
+   -iso-level 3 \
+   -full-iso9660-filenames \
+   -volid "Ubuntu Custom" \
+   -output "../ubuntu-20.04.3-live-server-salt-amd64.iso" \
+   -eltorito-boot boot/grub/bios.img \
+      -no-emul-boot \
+      -boot-load-size 4 \
+      -boot-info-table \
+      --eltorito-catalog boot/grub/boot.cat \
+      --grub2-boot-info \
+      --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
+   -eltorito-alt-boot \
+      -e EFI/efiboot.img \
+      -no-emul-boot \
+   -append_partition 2 0xef EFI/efiboot.img \
+   -m "EFI/efiboot.img" \
+   -m "EFI/bios.img" \
+   -graft-points \
+      "/EFI/efiboot.img=EFI/efiboot.img" \
+      "/boot/grub/bios.img=boot/grub/bios.img" \
+      "."
+```
+## New bootable iso should be available in /tmp/
---
+++ Ubuntu-20.04/scripts/scripts/default_config.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# This script provides common customization options for the ISO
+# 
+# Usage: Copy this file to config.sh and make changes there.  Keep this file (default_config.sh) as-is
+#   so that subsequent changes can be easily merged from upstream.  Keep all customiations in config.sh
+
+# The version of Ubuntu to generate.  Successfully tested: bionic, cosmic, disco, eoan, focal, groovy
+# See https://wiki.ubuntu.com/DevelopmentCodeNames for details
+export TARGET_UBUNTU_VERSION="focal"
+
+# The packaged version of the Linux kernel to install on target image.
+# See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
+export TARGET_KERNEL_PACKAGE="linux-generic"
+
+# The file (no extension) of the ISO containing the generated disk image,
+# the volume id, and the hostname of the live environment are set from this name.
+export TARGET_NAME="ubuntu-20.04.3-live-salt-x64.iso"
+
+# The text label shown in GRUB for booting into the live environment
+export GRUB_LIVEBOOT_LABEL="Try Ubuntu FS without installing"
+
+# The text label shown in GRUB for starting installation
+export GRUB_INSTALL_LABEL="Install Ubuntu FS"
+
+# Packages to be removed from the target system after installation completes succesfully
+export TARGET_PACKAGE_REMOVE="
+    ubiquity \
+    casper \
+    discover \
+    laptop-detect \
+    os-prober \
+"
+
+# Package customisation function.  Update this function to customize packages
+# present on the installed system.
+function customize_image() {
+    # useful tools
+    apt-get install -y \
+    apt-transport-https \
+    curl \
+    make \
+    gcc	\
+    nano \
+    less
+ 
+    apt -y install linux-headers-$(uname -r)    
+
+}
+
+# Used to version the configuration.  If breaking changes occur, manual
+# updates to this file from the default may be necessary.
+export CONFIG_FILE_VERSION="0.3"
